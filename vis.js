c77dea8cb91bca1b14418d4626ba7bcc9abae159
Emmanuel Bustos <ema2159@gmail.com>
2021-05-17 18:12:15 +0200

    Mod: Start visualization from scratch using d3 v6

--- vis.js
+++ vis.js
@@ -1,43 +1,43 @@
-import "https://d3js.org/d3.v5.min.js";
-import "https://raw.githack.com/jamesleesaunders/d3-ez/master/dist/d3-ez.js";
+import "https://d3js.org/d3.v6.min.js";
 
-let country = "Argentina";
-let year = "2000";
+// Plot constants
+const MARGIN = {LEFT: 100, RIGHT: 20, TOP: 20, BOTTOM: 100};
+const WIDTH = 700 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
 
-let chart = d3.ez.chart
-  .polarAreaChart()
-  .colors(d3.ez.palette.sequential("#0096c7", 3));
-let legend = d3.ez.component.legend().title("Month");
-let title = d3.ez.component
-  .title()
-  .mainText(`Monthly average temperature in ${country}`)
-  .subText(`Year ${year}`);
+// Visualization canvas
+const svg = d3
+  .select("#areaChart")
+  .append("svg")
+  .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+  .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
+  .attr("style", "background-color: gray");
 
-// Create chart base
-let myChart = d3.ez
-  .base()
-  .width(750)
-  .height(800)
-  .chart(chart)
-  .title(title)
-  .on("customValueMouseOver", function (d, i) {
-    d3.select("#message").text(d.value);
-  });
+const g = svg
+  .append("g")
+  .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
-// Add to page
-function updatePolarChart() {
-  d3.csv("data/temp-1901-2020-all.csv", function (d) {
-    if (d["Country"] == country && d["Year"] == year) {
-      return {key: d["Statistics"].slice(0, -8), value: d["Temperature"]};
-    }
-  }).then(function (d) {
-    console.log(d.slice(0, -1));
-    let data = {key: "Month", values: d.slice(0, d.length)};
-    console.log(data);
-    d3.select("#chartholder").datum(data).call(myChart);
-    document.querySelector('.creditbox').style.display = 'none';
-  });
-}
+// Labels
+const xLabel = g
+  .append("text")
+  .attr("class", "x-label")
+  .attr("x", WIDTH / 2)
+  .attr("y", HEIGHT + 40)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text(`Year`);
 
-updatePolarChart();
-// setInterval(update, 2000);
+const yLabel = g
+  .append("text")
+  .attr("class", "y-label")
+  .attr("x", -HEIGHT / 2)
+  .attr("y", -30)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Temperature (Celsius)");
+
+d3.csv("data/temp-1901-2020-all.csv").then(function (data) {
+  data = d3.group(data, (d) => d.Country);
+  console.log(data);
+});
